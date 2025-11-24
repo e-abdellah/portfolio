379b966ed0c27cc8fbf8427c49c1d67a6501f4c1
Abde <abdellah.elhalimimerroun@student.hogent.be>
2025-11-24 20:23:02 +0100

    add theme toggle component and initialize theme on load

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,ts,jsx,tsx}'],
+  darkMode: "class",
+  content: ["./src/**/*.{astro,html,js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        display: ['Inter', 'ui-sans-serif', 'system-ui', 'sans-serif'],
-      }
+        display: ["Inter", "ui-sans-serif", "system-ui", "sans-serif"],
+      },
     },
   },
   plugins: [],
-}
+};
